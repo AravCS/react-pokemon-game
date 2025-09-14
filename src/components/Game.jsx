a030b8aba8753dc796c77dda8d3d247c36164f1c
AravShah1 <ashah69@student.ohlone.edu>
2025-09-13 21:26:55 -0700

    refactoring with tailwind

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -114,7 +114,7 @@ export default function Game() {
     }
 
     return (
-        <div class>
+        <div>
             <div class="font-extrabold text-xl mt-2 flex flex-col items-center justify-center">
                 <div>Score: {clickCount}</div>
                 <div>High Score: {highScore}</div>
@@ -122,7 +122,7 @@ export default function Game() {
             <div class="flex items-center justify-center w-full">
             <div class="grid grid-cols-2 md:grid-cols-3 mt-10 gap-y-8 justify-items-center w-lg">
                 {pokemonData.map((item) => (
-                    <button class="flex flex-col items-center justify-center h-[150px] w-[150px] border-5 border-amber-500 rounded-xl bg-white shadow-2xl cursor-pointer text-md hover:-translate-y-1/12" key={item.id} onClick={() => handleClick(item)}>
+                    <button class="flex flex-col items-center justify-center h-[150px] w-[150px] border-5 border-amber-500 rounded-xl bg-white shadow-2xl cursor-pointer text-md hover:-translate-y-1/12 transition-all duration-150" key={item.id} onClick={() => handleClick(item)}>
                         <img src={item.url} alt="pokemon card"/>
                         <div class="font-bold">{item.name.toUpperCase()}</div>
                     </button>
